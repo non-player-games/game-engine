eb8b600b2f4de885e02989f80af2b47412bf5227
sam kim <seungkim11@gmail.com>
2016-06-28 15:10:19 -0700

    initializing the server

---
+++ src/main/java/game_engine/Application.java
@@ -0,0 +1,16 @@
+package game_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by samskim on 6/28/16.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args){
+    SpringApplication.run(Application.class, args);
+  }
+
+}
---
+++ src/main/java/game_engine/model/Player.java
@@ -0,0 +1,15 @@
+package game_engine.model;
+
+/**
+ * Created by samskim on 6/28/16.
+ */
+public class Player {
+
+  private final long id;
+  private int elo;
+
+  public Player(long id){
+    this.id = id;
+  }
+
+}
